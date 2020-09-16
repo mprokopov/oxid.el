15d3f1c09d400a1d824d8480e6023ff90e78465a
Maksym Prokopov <maksym.prokopov@oxid-esales.com>
2020-09-16 16:13:44 +0200

    added babashka script to read module name from yaml file

---
+++ module-autocomplete.clj
@@ -0,0 +1,17 @@
+#!/usr/bin/env bb
+
+
+(def project-root (first *command-line-args*))
+(def path (str project-root "/var/configuration/shops/1.yaml"))
+
+(def modules 
+(->>
+  (yaml/parse-string 
+    (slurp path))
+    :modules
+    (map first)
+    (map #(subs (str %) 1))))
+
+
+(doseq [m modules]
+ (println m))
